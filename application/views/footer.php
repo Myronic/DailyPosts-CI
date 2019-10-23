dc89c1536324c83418bb718758c32d2b5d0faee3
Prajwal Keshav Kotian <32770209+pk2k14@users.noreply.github.com>
2019-10-23 11:24:29 +0530

    Update footer.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,10 @@
 <!-- footer.php -->
 <div class="fixed-bottom">
 	<div class="row text-center bg-success">
+		<div class="col text-light">Visit us at github.com/Myronic</div>
+	</div>
+	
+	<div class="row text-center bg-success">
 		<div class="col text-light">built using CodeIgniter</div>
 	</div>
 	<div class="row text-center bg-success">
